4dab5bb135235a517a42a64ff37c4c7013490a39
respect318 <ferid.mirzezade.education@gmail.com>
2025-11-10 12:50:57 +0400

    alqoritm

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    if not boxes or len(boxes) == 0:
+        return False
+
+    opened = [0]
+    keys = [0]
+
+    while keys:
+        box = keys.pop()
+        for key in boxes[box]:
+            if key < len(boxes) and key not in opened:
+                opened.append(key)
+                keys.append(key)
+
+    return len(opened) == len(boxes)
+
